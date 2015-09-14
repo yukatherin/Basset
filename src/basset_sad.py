04d92a612102dcfca94a4ead72722a4d6cb24099
David Kelley <davidkelley44@gmail.com>
2015-09-14 18:31:37 -0400

    rewrite

--- src/basset_sad.py
+++ src/basset_sad.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 from optparse import OptionParser
-import glob, os, subprocess, time
+import os, subprocess
 
 import h5py
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import pysam
 import seaborn as sns
 
 from dna_io import dna_one_hot
+import vcf
 
 ################################################################################
 # basset_sad.py
@@ -26,7 +26,10 @@ def main():
     parser.add_option('-f', dest='genome_fasta', default='%s/data/genomes/hg19.fa'%os.environ['BASSETDIR'], help='Genome FASTA from which sequences will be drawn [Default: %default]')
     parser.add_option('-i', dest='index_snp', default=False, action='store_true', help='SNPs are labeled with their index SNP as column 6 [Default: %default]')
     parser.add_option('-l', dest='seq_len', type='int', default=600, help='Sequence length provided to the model [Default: %default]')
+    parser.add_option('-m', dest='min_limit', default=0.1, type='float', help='Minimum heatmap limit [Default: %default]')
     parser.add_option('-o', dest='out_dir', default='sad', help='Output directory for tables and plots [Default: %default]')
+    parser.add_option('-s', dest='score', default=False, action='store_true', help='SNPs are labeled with scores as column 7 [Default: %default]')
+    parser.add_option('-t', dest='torch_out_file', default=None, help='Output filename from Torch if already run [Default: %default]')
     (options,args) = parser.parse_args()
 
     if len(args) != 2:
@@ -39,101 +42,52 @@ def main():
         os.mkdir(options.out_dir)
 
     #################################################################
+    # prep SNP sequences
+    #################################################################
     # load SNPs
-    #################################################################
-    vcf_in = open(vcf_file)
+    snps = vcf.vcf_snps(vcf_file, options.index_snp, options.score)
 
-    # read through header
-    line = vcf_in.readline()
-    while line[0] == '#':
-        line = vcf_in.readline()
+    if options.torch_out_file is None:
+        # get one hot coded input sequences
+        seq_vecs, seqs, seq_headers = vcf.snps_seq1(snps, options.genome_fasta, options.seq_len)
 
-    # read in SNPs
-    snps = []
-    for line in vcf_in:
-        snps.append(SNP(line))
-
-    #################################################################
-    # prep input sequences
-    #################################################################
-    left_len = options.seq_len/2 - 1
-    right_len = options.seq_len/2
-
-    # open genome FASTA
-    genome = pysam.Fastafile(options.genome_fasta)
-
-    # initialize one hot coded vector list
-    seq_vecs_list = []
-
-    # name sequences
-    seq_headers = []
-
-    for snp in snps:
-        # specify positions in GFF-style 1-based
-        seq_start = snp.pos - left_len
-        seq_end = snp.pos + right_len + len(snp.ref_allele) - snp.longest_alt()
-
-        # extract sequence as BED style
-        seq = genome.fetch(snp.chrom, seq_start-1, seq_end)
-
-        # verify that ref allele matches ref sequence
-        seq_ref = seq[left_len:left_len+len(snp.ref_allele)]
-        if seq_ref != snp.ref_allele:
-            print >> sys.stderr, 'WARNING: skipping %s because reference allele does not match reference genome: %s vs %s' % (snp.rsid, snp.ref_allele, seq_ref)
-            continue
-
-        # one hot code ref allele
-        seq_vecs_list.append(dna_one_hot(seq[:options.seq_len], options.seq_len))
-
-        # name ref allele
-        seq_headers.append('%s_%s' % (snp.rsid, cap_allele(snp.ref_allele)))
-
-        for alt_al in snp.alt_alleles:
-            # remove ref allele and include alt allele
-            seq_alt = seq[:left_len] + alt_al + seq[left_len+len(snp.ref_allele):]
-
-            # one hot code
-            seq_vecs_list.append(dna_one_hot(seq_alt, options.seq_len))
-
-            # name
-            seq_headers.append('%s_%s' % (snp.rsid, cap_allele(alt_al)))
-
-    # stack
-    seq_vecs = np.vstack(seq_vecs_list)
-
-    # write to HDF5
-    h5f = h5py.File('%s/model_in.h5'%options.out_dir, 'w')
-    h5f.create_dataset('test_in', data=seq_vecs)
-    h5f.close()
+        # write to HDF5
+        h5f = h5py.File('%s/model_in.h5'%options.out_dir, 'w')
+        h5f.create_dataset('test_in', data=seq_vecs)
+        h5f.close()
 
 
     #################################################################
     # predict in Torch
     #################################################################
-    subprocess.call('basset_predict.lua -norm %s %s/model_in.h5 %s/model_out.txt' % (model_th, options.out_dir, options.out_dir), shell=True)
+    if options.torch_out_file is None:
+        options.torch_out_file = '%s/model_out.txt' % options.out_dir
+        subprocess.call('basset_predict.lua -norm %s %s/model_in.h5 %s' % (model_th, options.out_dir, options.torch_out_file), shell=True)
 
     # read in predictions
     seq_preds = []
-    for line in open('%s/model_out.txt' % options.out_dir):
+    for line in open(options.torch_out_file):
         seq_preds.append(np.array([float(p) for p in line.split()]))
     seq_preds = np.array(seq_preds)
 
+    # TEMP! TMP! once the new models with labels finish, use this
+    #            and update downstream code to reflect the panda
+    #            and use the target labels.
+    # seq_preds = pd.read_csv('%s/model_out.txt', sep='\t')
+
 
     #################################################################
-    # plot raw predictions
+    # collect and print SADs
     #################################################################
-    sns.set(style='white', font_scale=30.0/seq_preds.shape[0])
-    plt.figure()
-    sns.heatmap(seq_preds, xticklabels=False, yticklabels=seq_headers)
-    plt.tight_layout()
-    plt.savefig('%s/raw_heat.pdf' % options.out_dir)
-    plt.close()
+    sad_out = open('%s/sad_table.txt' % options.out_dir, 'w')
 
-    #################################################################
-    # plot SAD
-    #################################################################
-    sad_matrix = []
-    sad_labels = []
+    header_cols = ('rsid', 'index', 'score', 'ref', 'alt', 'cell', 'ref_pred', 'alt pred', 'sad')
+    print >> sad_out, ' '.join(header_cols)
+
+    # hash by index snp
+    sad_matrices = {}
+    sad_labels = {}
+    sad_scores = {}
 
     pi = 0
     for snp in snps:
@@ -141,68 +95,70 @@ def main():
         ref_preds = seq_preds[pi,:]
         pi += 1
 
-        for allele in snp.alt_alleles:
+        for alt_al in snp.alt_alleles:
             # get alternate prediction
             alt_preds = seq_preds[pi,:]
             pi += 1
 
             # normalize by reference
             alt_sad = alt_preds - ref_preds
-            sad_matrix.append(alt_sad)
+            sad_matrices.setdefault(snp.index_snp,[]).append(alt_sad)
 
             # label as mutation from reference
-            alt_label = '%s_%s>%s' % (snp.rsid, cap_allele(snp.ref_allele), cap_allele(allele))
-            sad_labels.append(alt_label)
+            alt_label = '%s_%s>%s' % (snp.rsid, vcf.cap_allele(snp.ref_allele), vcf.cap_allele(alt_al))
+            sad_labels.setdefault(snp.index_snp,[]).append(alt_label)
 
-    # convert fully to numpy array
-    sad_matrix = np.array(sad_matrix)
+            # save scores
+            sad_scores.setdefault(snp.index_snp,[]).append(snp.score)
 
-    # plot heatmap
-    sns.set(style='white', font_scale=30.0/sad_matrix.shape[0])
-    plt.figure()
-    sns.heatmap(sad_matrix, xticklabels=False, yticklabels=sad_labels)
-    plt.tight_layout()
-    plt.savefig('%s/sad_heat.pdf' % options.out_dir)
-    plt.close()
+            # print table lines
+            for ti in range(len(alt_sad)):
+                if options.index_snp and options.score:
+                    cols = (snp.rsid, snp.index_snp, snp.score, vcf.cap_allele(snp.ref_allele), vcf.cap_allele(alt_al), ti, ref_preds[ti], alt_preds[ti], alt_sad[ti])
+                    print >> sad_out, '%-13s %-13s %5.3f %6s %6s %3d %6.4f %6.4f %7.4f' % cols
+                elif options.index_snp:
+                    cols = (snp.rsid, snp.index_snp, vcf.cap_allele(snp.ref_allele), vcf.cap_allele(alt_al), ti, ref_preds[ti], alt_preds[ti], alt_sad[ti])
+                    print >> sad_out, '%-13s %-13s %6s %6s %3d %6.4f %6.4f %7.4f' % cols
+                elif options.score:
+                    cols = (snp.rsid, snp.score, vcf.cap_allele(snp.ref_allele), vcf.cap_allele(alt_al), ti, ref_preds[ti], alt_preds[ti], alt_sad[ti])
+                    print >> sad_out, '%-13s %5.3f %6s %6s %3d %6.4f %6.4f %7.4f' % cols
+                else:
+                    cols = (snp.rsid, vcf.cap_allele(snp.ref_allele), vcf.cap_allele(alt_al), ti, ref_preds[ti], alt_preds[ti], alt_sad[ti])
+                    print >> sad_out, '%-13s %6s %6s %3d %6.4f %6.4f %7.4f' % cols
+
+    sad_out.close()
 
 
-def cap_allele(allele, cap=5):
-    ''' Cap the length of an allele in the figures '''
-    if len(allele) > cap:
-        allele = allele[:cap] + '*'
-    return allele
+    #################################################################
+    # plot SAD heatmaps
+    #################################################################
+    for ii in sad_matrices:
+        # convert fully to numpy arrays
+        sad_matrix = abs(np.array(sad_matrices[ii]))
+        print ii, sad_matrix.shape
 
+        if sad_matrix.shape[0] > 1:
+            # plot heatmap
+            plt.figure(figsize=(20, 0.5*sad_matrix.shape[0]))
 
-class SNP:
-    ''' SNP
+            # lay out scores
+            cols = 12
+            ax_score = plt.subplot2grid((1,cols), (0,0))
+            ax_sad = plt.subplot2grid((1,cols), (0,1), colspan=(cols-1))
 
-    Represent SNPs read in from a VCF file
+            score_mat = np.reshape(np.array(sad_scores[ii]), (-1, 1))
+            sns.heatmap(score_mat, xticklabels=False, yticklabels=False, vmin=0, vmax=1, cmap='Reds', cbar=False, ax=ax_score)
 
-    Attributes:
-        vcf_line (str)
-    '''
-    def __init__(self, vcf_line):
-        a = vcf_line.split()
-        if a[0].startswith('chr'):
-            self.chrom = a[0]
-        else:
-            self.chrom = 'chr%s' % a[0]
-        self.pos = int(a[1])
-        self.rsid = a[2]
-        self.ref_allele = a[3]
-        self.alt_alleles = a[4].split(',')
-        self.index_snp = None
-        if len(a) >= 6:
-            self.index_snp = a[5]
+            vlim = max(options.min_limit, sad_matrix.max())
+            sns.heatmap(sad_matrix, xticklabels=False, yticklabels=sad_labels[ii], vmin=0, vmax=vlim, ax=ax_sad)
 
-    def get_alleles(self):
-        ''' Return a list of all alleles '''
-        alleles = [self.ref_allele] + self.alt_alleles
-        return alleles
-
-    def longest_alt(self):
-        ''' Return the longest alt allele. '''
-        return max([len(al) for al in self.alt_alleles])
+            plt.tight_layout()
+            if ii == '.':
+                out_pdf = '%s/sad_heat.pdf' % options.out_dir
+            else:
+                out_pdf = '%s/sad_%s_heat.pdf' % (options.out_dir, ii)
+            plt.savefig(out_pdf)
+            plt.close()
 
 
 ################################################################################
