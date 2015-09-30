80ac57100ed19ddda6c53e07f756d6e8603cf214
David Kelley <davidkelley44@gmail.com>
2015-09-30 17:28:11 -0400

    update

--- install_data.py
+++ install_data.py
@@ -2,9 +2,10 @@
 from optparse import OptionParser
 import os
 import subprocess
+import sys
 
 ################################################################################
-# install.py
+# install_data.py
 #
 # Download and arrange pre-trained models and data.
 ################################################################################
@@ -16,7 +17,7 @@ import subprocess
 def main():
     usage = 'usage: %prog [options] arg'
     parser = OptionParser(usage)
-    #parser.add_option()
+    parser.add_option('-r', dest='restart', default=False, action='store_true', help='Do not overwrite existing files, as if restarting an aborted installation [Default: %default]')
     (options,args) = parser.parse_args()
 
     os.chdir('data')
@@ -26,11 +27,46 @@ def main():
     ############################################################
     os.chdir('models')
 
-    cmd = 'wget https://www.dropbox.com/s/rguytuztemctkf8/pretrained_model.th.gz?dl=0'
-    subprocess.call(cmd, shell=True)
+    if not options.restart or not os.path.isfile('pretrained_model.th'):
+        print >> sys.stderr, 'Downloading pre-trained model.
 
-    cmd = 'gunzip pretrained_model.th.gz'
-    subprocess.call(cmd, shell=True)
+        cmd = 'wget https://www.dropbox.com/s/rguytuztemctkf8/pretrained_model.th.gz?dl=0'
+        subprocess.call(cmd, shell=True)
+
+        cmd = 'gunzip pretrained_model.th.gz'
+        subprocess.call(cmd, shell=True)
+
+    os.chdir('..')
+
+
+    ############################################################
+    # download human genome
+    ############################################################
+    os.chdir('genomes')
+
+    if not options.restart or not os.path.isfile('hg19.fa'):
+        print >> sys.stderr, 'Downloading hg19 FASTA from UCSC. If you already have it, CTL-C to place a sym link in the genomes directory named hg19.fa'
+
+        # download hg19
+        cmd = 'wget ftp://hgdownload.cse.ucsc.edu/goldenPath/hg19/bigZips/chromFa.tar.gz -O chromFa.tar.gz'
+        subprocess.call(cmd, shell=True)
+
+        # un-tar
+        cmd = 'tar -xzvf chromFa.tar.gz'
+        subprocess.call(cmd, shell=True)
+
+        # cat
+        cmd = 'cat chr?.fa chr??.fa > hg19.fa'
+        subprocess.call(cmd, shell=True)
+
+        # clean up
+        os.remove('chromFa.tar.gz')
+        for chrom_fa in glob.glob('chr*.fa'):
+            os.remove(chrom_fa)
+
+    if not options.restart or not os.path.isfile('hg19.fa.fai'):
+        cmd = 'samtools faidx genomes/hg19.fa'
+        subprocess.call(cmd, shell=True)
 
     os.chdir('..')
 
@@ -38,21 +74,22 @@ def main():
     ############################################################
     # download and prepare public data
     ############################################################
-    # download and arrange available data
-    cmd = './get_dnase.sh'
-    subprocess.call(cmd, shell=True)
+    if not options.restart or not os.path.isfile('encode_roadmap.h5')
+        # download and arrange available data
+        cmd = './get_dnase.sh'
+        subprocess.call(cmd, shell=True)
 
-    # preprocess
-    cmd = 'preprocess_features.py -y -m 200 -s 600 -o encode_roadmap -c /Users/davidkelley/research/common/data/genomes/hg19/assembly/human.hg19.genome sample_beds.txt'
-    subprocess.call(cmd, shell=True)
+        # preprocess
+        cmd = 'preprocess_features.py -y -m 200 -s 600 -o encode_roadmap -c genomes/hg19.fa sample_beds.txt'
+        subprocess.call(cmd, shell=True)
 
-    # make a FASTA file
-    cmd = 'bedtools getfasta -fi $HG19/sequence/hg19.fa -bed encode_roadmap.bed -s -fo encode_roadmap.fa'
-    subprocess.call(cmd, shell=True)
+        # make a FASTA file
+        cmd = 'bedtools getfasta -fi $HG19/sequence/hg19.fa -bed encode_roadmap.bed -s -fo encode_roadmap.fa'
+        subprocess.call(cmd, shell=True)
 
-    # make an HDF5 file
-    cmd = 'seq_hdf5.py -c -r -t 71886 -v 70000 encode_roadmap.fa encode_roadmap_act.txt encode_roadmap.h5'
-    subprocess.call(cmd, shell=True)
+        # make an HDF5 file
+        cmd = 'seq_hdf5.py -c -r -t 71886 -v 70000 encode_roadmap.fa encode_roadmap_act.txt encode_roadmap.h5'
+        subprocess.call(cmd, shell=True)
 
 
 ################################################################################
