2d941649843676dde7663a8c421b92b034858cdc
David Kelley <davidkelley44@gmail.com>
2015-11-24 10:10:35 -0500

    single target bug

--- src/postprocess.lua
+++ src/postprocess.lua
@@ -1,8 +1,13 @@
 -- Troyanskaya's normalization
 function troy_norm(p, u)
-    local pn = torch.log(torch.cdiv(p,(-p+1)))
     local xn = math.log(0.05/0.95)
-    local un = torch.log(torch.cdiv(u,-u+1)):repeatTensor((#p)[1], 1)
+    local pn = torch.log(torch.cdiv(p,(-p+1)))
+    local un
+    if type(u) == "number" then
+        un = torch.Tensor((#p)[1],1):fill(torch.log(u/(-u+1)))
+    else
+        un = torch.log(torch.cdiv(u,-u+1)):repeatTensor((#p)[1], 1)
+    end
     local zn = pn + xn - un
     return (torch.exp(-zn)+1):pow(-1)
 end
