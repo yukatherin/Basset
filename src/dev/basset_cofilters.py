58636712e3d5cbb48c76da952d547ba4335f4439
David Kelley <davidkelley44@gmail.com>
2016-01-19 18:05:56 -0500

    meh analysis of filter interactions

---
+++ src/dev/basset_cofilters.py
@@ -0,0 +1,287 @@
+#!/usr/bin/env python
+from optparse import OptionParser
+import copy, math, os, pdb, random, subprocess, sys, time
+
+import h5py
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+import pandas as pd
+import seaborn as sns
+from scipy.stats import spearmanr, pearsonr
+from sklearn.linear_model import Ridge
+
+import dna_io
+from seq_logo import seq_logo
+
+################################################################################
+# basset_cofilters.py
+#
+################################################################################
+
+################################################################################
+# main
+################################################################################
+def main():
+    usage = 'usage: %prog [options] <model_file> <test_hdf5_file>'
+    parser = OptionParser(usage)
+    parser.add_option('-a', dest='act_t', default=0.5, type='float', help='Activation threshold as a percentage of the max used to anchor a filter occurrence [Default: %default]')
+    parser.add_option('-d', dest='model_hdf5_file', default=None, help='Pre-computed model output as HDF5 [Default: %default]')
+    parser.add_option('-f', dest='filters', default='-1', help='Comma-separated list of filter indexes to plot (or -1 for all) [Default: %default]')
+    parser.add_option('-e', dest='draw_heat', default=False, action='store_true', help='Draw heat maps [Default: %default]')
+    parser.add_option('-m', dest='motifs_sample', default=1000, type='int', help='Sample motif occurrences for the heatmaps [Default: %default]')
+    parser.add_option('-n', dest='center_nt', default=50, type='int', help='Center nt to mutate and plot in the heat map [Default: %default]')
+    parser.add_option('-o', dest='out_dir', default='heat', help='Output directory [Default: %default]')
+    parser.add_option('-p', dest='pool', default=4, type='int', help='Pool adjacent positions of filter outputs [Default: %default]')
+    parser.add_option('-s', dest='sample', default=30000, type='int', help='Sample sequences from the test set [Default:%default]')
+    parser.add_option('-t', dest='targets', default='0', help='Comma-separated list of target indexes to plot (or -1 for all) [Default: %default]')
+    (options,args) = parser.parse_args()
+
+    if len(args) != 2:
+        parser.error('Must provide Basset model file and test data in an HDF file')
+    else:
+        model_file = args[0]
+        test_hdf5_file = args[1]
+
+    if not os.path.isdir(options.out_dir):
+        os.mkdir(options.out_dir)
+
+    random.seed(1)
+
+    #################################################################
+    # parse input file
+    #################################################################
+    # load (sampled) test data from HDF5
+    hdf5_in = h5py.File(test_hdf5_file, 'r')
+    seqs_1hot = np.array(hdf5_in['test_in'])
+    targets = np.array(hdf5_in['test_out'])
+    try: # TEMP
+        seq_headers = np.array(hdf5_in['test_headers'])
+        target_labels = np.array(hdf5_in['target_labels'])
+    except:
+        seq_headers = None
+        target_labels = None
+    hdf5_in.close()
+
+    # sample
+    if options.sample:
+        sample_i = np.array(random.sample(xrange(seqs_1hot.shape[0]), options.sample))
+        seqs_1hot = seqs_1hot[sample_i]
+        seq_headers = seq_headers[sample_i]
+        targets = targets[sample_i]
+
+        # write sampled data to a new HDF5 file
+        test_hdf5_file = '%s/model_in.h5'%options.out_dir
+        h5f = h5py.File(test_hdf5_file, 'w')
+        h5f.create_dataset('test_in', data=seqs_1hot)
+        h5f.create_dataset('test_out', data=targets)
+        h5f.close()
+
+    # convert to ACGT sequences
+    # seqs = dna_io.vecs2dna(seqs_1hot)
+
+
+    #################################################################
+    # Torch predict modifications
+    #################################################################
+    if options.model_hdf5_file is None:
+        options.model_hdf5_file = '%s/model_out.h5' % options.out_dir
+        torch_cmd = 'basset_cofilters_predict.lua -norm -pool %d %s %s %s' % (options.pool, model_file, test_hdf5_file, options.model_hdf5_file)
+        print torch_cmd
+        subprocess.call(torch_cmd, shell=True)
+
+
+    #################################################################
+    # load Torch output
+    #################################################################
+    hdf5_in = h5py.File(options.model_hdf5_file, 'r')
+    preds = np.array(hdf5_in['preds'])
+    filter_outs = np.array(hdf5_in['filter_outs'])
+    hdf5_in.close()
+
+    num_seqs = filter_outs.shape[0]
+    num_filters = filter_outs.shape[1]
+    pool_len = filter_outs.shape[2]
+    num_targets = targets.shape[1]
+
+
+    #################################################################
+    # classify sequences by middle filters
+    #################################################################
+    # determine the threshold to call an occurrence
+    filter_means = filter_outs.mean(axis=2).mean(axis=0)
+    filter_maxes = filter_outs.max(axis=2).max(axis=0)
+    filter_thresh = options.act_t*(filter_maxes - filter_means) + filter_means
+
+    # map out sequence centers
+    seq_len = pool_len*options.pool
+    sides_len = max(0, seq_len-options.center_nt)
+    center_start = int(math.floor(sides_len/2 / options.pool))
+    center_end = int(math.ceil((sides_len/2 + options.center_nt) / options.pool))
+    filter_outs_cmax = filter_outs[:,:,center_start:center_end].max(axis=2)
+
+    # initialize lists of sequences with each filter
+    filter_seqs = []
+    for fi in range(num_filters):
+        filter_seqs.append([])
+
+    # fill in lists of sequences with each filter
+    for si in range(num_seqs):
+        for fi in range(num_filters):
+            if filter_outs_cmax[si,fi] > filter_thresh[fi]:
+                filter_seqs[fi].append(si)
+
+    # sample update to array for easier slicing
+    for fi in range(num_filters):
+        if len(filter_seqs[fi]) > options.motifs_sample:
+            filter_seqs[fi] = np.array(random.sample(filter_seqs[fi], options.motifs_sample))
+        else:
+            filter_seqs[fi] = np.array(filter_seqs[fi])
+
+
+    #################################################################
+    # study filters
+    #################################################################
+    if options.filters == '-1':
+        study_filters = range(num_filters)
+    else:
+        study_filters = [int(fi) for fi in options.filters.split(',')]
+
+    if options.targets == '-1':
+        study_targets = range(num_targets)
+    else:
+        study_targets = [int(t) for t in options.targets.split(',')]
+
+    preds_means = preds.mean(axis=0)
+
+    table_out = open('%s/filter_target_anchor.txt' % options.out_dir, 'w')
+    table2_out = open('%s/filter2_target_anchor.txt' % options.out_dir, 'w')
+
+    for ti in study_targets:
+        # pre-compute correlations between the filter's mean
+        #  activation and the predictions for this target.
+        tpreds = preds[:,ti]
+        fcors = np.zeros(num_filters)
+        for fi in range(num_filters):
+            fouts_mean = filter_outs[:,fi,:].mean(axis=1)
+            cor, p = pearsonr(fouts_mean, tpreds)
+            fcors[fi] = cor
+
+        # save matrix of conditional correlations
+        ff_cor = np.zeros((num_filters,num_filters))
+
+        for fi in study_filters:
+            print 'filter%d (%d)' % (fi,len(filter_seqs[fi]))
+            if len(filter_seqs[fi]) > 10:
+                ###########################################
+                # measure filter influence
+                ###########################################
+                fa_outs = filter_outs[filter_seqs[fi],fi,:]
+                fa_preds = preds[filter_seqs[fi],ti]
+                fa_targets = targets[filter_seqs[fi],ti]
+
+                fa_preds_mean = fa_preds.mean()
+
+                cols = (fi, ti, len(fa_preds), fa_preds_mean, preds_means[ti], fa_preds_mean-preds_means[ti])
+                print >> table_out, '%3d  %3d  %6d  %.4f  %.4f  %7.4f' % cols
+
+
+                ###########################################
+                # plot the filter activations against
+                #  predictions
+                ###########################################
+                if options.draw_heat:
+                    # sort sequences by predictions
+                    heat_indexes = np.argsort(fa_preds)[::-1]
+
+                    # plot activation heatmap
+                    plt.figure(figsize=(4,12))
+                    sns.heatmap(fa_outs[heat_indexes], xticklabels=False, yticklabels=False)
+                    plt.savefig('%s/f%d_t%d_fheat.pdf' % (options.out_dir,fi,ti))
+                    plt.close()
+
+                    pt = np.vstack([fa_targets[heat_indexes], fa_preds[heat_indexes]]).T
+
+                    # plot targets and predictions heatmap
+                    plt.figure(figsize=(0.5,12))
+                    sns.heatmap(pt, xticklabels=False, yticklabels=False)
+                    plt.savefig('%s/f%d_t%d_pheat.pdf' % (options.out_dir,fi,ti))
+                    plt.close()
+
+
+                    # could try for a meta-plot where we plot the
+                    # correlation of activation at each position
+                    # with the predictions.
+
+
+                ###########################################
+                # compute filter interaction stats
+                ###########################################
+                # undo the sigmoid
+                fa_z = -np.log(fa_preds**-1 - 1)
+
+                # take the filter activation mean across the sequences
+                fa_outs_mean = fa_outs.mean(axis=1).squeeze()
+
+                # scatter plot
+                plt.figure()
+                sns.jointplot(fa_outs_mean, fa_z)
+                plt.savefig('%s/f%d_t%d_lm.pdf' % (options.out_dir,fi,ti))
+                plt.close()
+
+                # estimate the accessibility from this filter
+                cor, p = pearsonr(fa_outs_mean, fa_z)
+
+                # measure accuracy
+                ff_cor[fi,fi] = cor
+
+                cols = (fi, fi, ti, cor, p, fcors[fi])
+                print >> table2_out, '%3d  %3d  %3d  %7.4f  %6.1e  %7.4f' % cols
+
+                # for each other filter
+                # TEMP TEMP
+                # for fi2 in range(num_filters):
+                for fi2 in [5]:
+                    if fi != fi2:
+                        # get filter2 outputs
+                        f2_outs = filter_outs[filter_seqs[fi],fi2,:]
+
+                        # take the mean
+                        f2_outs_mean = f2_outs.mean(axis=1)# .reshape(-1,1)
+
+                        # model
+                        cor, p = pearsonr(f2_outs_mean, fa_z)
+                        ff_cor[fi,fi2] = cor
+
+                        cols = (fi, fi2, ti, cor, p, fcors[fi2])
+                        print >> table2_out, '%3d  %3d  %3d  %7.4f  %6.1e  %7.4f' % cols
+
+                        if options.draw_heat:
+                            # plot activation heatmap
+                            plt.figure(figsize=(4,12))
+                            sns.heatmap(f2_outs[heat_indexes], xticklabels=False, yticklabels=False)
+                            plt.savefig('%s/f%d_t%d_f%d_fheat.pdf' % (options.out_dir,fi,ti,fi2))
+                            plt.close()
+
+                            plt.figure()
+                            sns.lmplot('f2_outs_mean', 'fa_z', pd.DataFrame({'f2_outs_mean':f2_outs_mean, 'fa_z':fa_z}))
+                            plt.savefig('%s/f%d_t%d_f%d_lm.pdf' % (options.out_dir, fi, ti, fi2))
+                            plt.close()
+
+        '''
+        plt.figure()
+        sns.clustermap(ff_r2)
+        plt.savefig('%s/ff_t%d_heat.pdf' % (options.out_dir,ti))
+        plt.close()
+        '''
+
+    table_out.close()
+    table2_out.close()
+
+
+################################################################################
+# __main__
+################################################################################
+if __name__ == '__main__':
+    main()
+    #pdb.runcall(main)
---
+++ src/dev/basset_cofilters_predict.lua
@@ -0,0 +1,115 @@
+#!/usr/bin/env th
+
+require 'hdf5'
+
+require 'postprocess'
+
+----------------------------------------------------------------
+-- parse arguments
+----------------------------------------------------------------
+cmd = torch.CmdLine()
+cmd:text()
+cmd:text('DNA ConvNet testing')
+cmd:text()
+cmd:text('Arguments')
+cmd:argument('model_file')
+cmd:argument('data_file')
+cmd:argument('out_file')
+cmd:text()
+cmd:text('Options:')
+cmd:option('-batch_size', 200, 'Batch size')
+cmd:option('-cuda', false, 'Run on GPGPU')
+cmd:option('-norm', false, 'Normalize all targets to a level plane')
+cmd:option('-pool', 10, 'Pool adjacent positions for filter outputs')
+cmd:text()
+opt = cmd:parse(arg)
+
+-- set cpu/gpu
+cuda = opt.cuda
+require 'convnet'
+
+----------------------------------------------------------------
+-- load data
+----------------------------------------------------------------
+local data_open = hdf5.open(opt.data_file, 'r')
+local test_seqs = data_open:read('test_in')
+local test_targets = data_open:read('test_out')
+
+local num_seqs = test_seqs:dataspaceSize()[1]
+local seq_len = test_seqs:dataspaceSize()[4]
+local num_targets = test_targets:dataspaceSize()[2]
+
+----------------------------------------------------------------
+-- construct model
+----------------------------------------------------------------
+-- initialize
+local convnet = ConvNet:__init()
+
+-- load from saved parameters
+local convnet_params = torch.load(opt.model_file)
+convnet:load(convnet_params)
+
+-- pooling
+local nn_pool = nn.SpatialMaxPooling(opt.pool, 1)
+local pseq_len = math.floor((seq_len-convnet.conv_filter_sizes[1]+1) / opt.pool)
+
+----------------------------------------------------------------
+-- predict and test
+----------------------------------------------------------------
+-- guarantee evaluate mode
+convnet.model:evaluate()
+
+-- track statistics across batches
+local preds = torch.Tensor(num_seqs, num_targets)
+local filter_outs = torch.Tensor(num_seqs, convnet.conv_filters[1], pseq_len)
+local si = 1
+
+-- initialize batcher
+local batcher = Batcher:__init(test_seqs, nil, opt.batch_size)
+
+-- next batch
+local Xb = batcher:next()
+
+-- while batches remain
+while Xb ~= nil do
+    -- cuda
+    if cuda then
+        Xb = Xb:cuda()
+    end
+
+    -- predict
+    local preds_batch = convnet.model:forward(Xb)
+
+    -- get filter outputs
+    local filter_outs_batch = convnet:get_nonlinearity(1).output:squeeze()
+
+    local filter_outs_pool = nn_pool:updateOutput(filter_outs_batch)
+
+    -- copy into full Tensors
+    for i = 1,(#preds_batch)[1] do
+        preds[{si,{}}] = preds_batch[{i,{}}]:float()
+        filter_outs[{si,{}}] = filter_outs_pool[{i,{}}]:float()
+        si = si + 1
+    end
+
+    -- next batch
+    Xb = batcher:next()
+
+    collectgarbage()
+end
+
+-- normalize
+if opt.norm then
+    preds = troy_norm(preds, convnet.pred_means)
+end
+
+-- close HDF5
+data_open:close()
+
+----------------------------------------------------------------
+-- dump to file
+----------------------------------------------------------------
+local hdf_out = hdf5.open(opt.out_file, 'w')
+hdf_out:write("preds", preds)
+hdf_out:write("filter_outs", filter_outs)
+hdf_out:close()
