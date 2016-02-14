2cc781ac4560435d5ff9e7d9e8addb869551c2a9
David Kelley <davidkelley44@gmail.com>
2016-02-14 18:13:12 -0500

    tabs

--- src/seq_logo.py
+++ src/seq_logo.py
@@ -23,57 +23,69 @@ def main():
     seq_logo(seq, heights, out_eps)
 
 
-def seq_logo(seq, heights, out_eps, weblogo_args=''):
-	# print the sequence to a temp fasta file
-	fasta_fd, fasta_file = tempfile.mkstemp()
-	fasta_out = open(fasta_file, 'w')
-	print >> fasta_out, '>seq\n%s' % seq
-	fasta_out.close()
+def seq_logo(seq, heights, out_eps, weblogo_args='', color_mode='classic'):
+    # print the sequence to a temp fasta file
+    fasta_fd, fasta_file = tempfile.mkstemp()
+    fasta_out = open(fasta_file, 'w')
+    print >> fasta_out, '>seq\n%s' % seq
+    fasta_out.close()
 
-	# print figure to a temp eps file
-	eps_fd, eps_file = tempfile.mkstemp()
-	weblogo_cmd = 'weblogo --errorbars NO --show-xaxis NO --show-yaxis NO --fineprint "" -c classic -n %d %s < %s > %s' % (len(seq), weblogo_args, fasta_file, eps_file)
-	subprocess.call(weblogo_cmd, shell=True)
+    # colors
+    color_str = '-c classic'
+    if color_mode == 'classic':
+        pass
+    elif color_mode == 'meme':
+        color_str = '--color red A "A"'
+        color_str += ' --color blue C "C"'
+        color_str += ' --color orange G "G"'
+        color_str += ' --color green T "T"'
+    else:
+        print >> sys.stderr, 'Unrecognized color_mode %s' % color_mode
 
-	# copy eps file over and write in my own heights
-	start_stack_re = re.compile('^\(\d*\) StartStack')
-	out_eps_open = open(out_eps, 'w')
-	weblogo_eps_in = open(eps_file)
-	line = weblogo_eps_in.readline()
-	si = 0
-	while line:
-		start_stack_match = start_stack_re.search(line)
+    # print figure to a temp eps file
+    eps_fd, eps_file = tempfile.mkstemp()
+    weblogo_cmd = 'weblogo --errorbars NO --show-xaxis NO --show-yaxis NO --fineprint "" %s -n %d %s < %s > %s' % (color_str, len(seq), weblogo_args, fasta_file, eps_file)
+    subprocess.call(weblogo_cmd, shell=True)
 
-		# nt column begins
-		if start_stack_match:
-			print >> out_eps_open, line,
+    # copy eps file over and write in my own heights
+    start_stack_re = re.compile('^\(\d*\) StartStack')
+    out_eps_open = open(out_eps, 'w')
+    weblogo_eps_in = open(eps_file)
+    line = weblogo_eps_in.readline()
+    si = 0
+    while line:
+        start_stack_match = start_stack_re.search(line)
 
-			# loop over 4 nt's
-			for i in range(4):
-				line = weblogo_eps_in.readline()
-				a = line.split()
+        # nt column begins
+        if start_stack_match:
+            print >> out_eps_open, line,
 
-				nt = a[2][1:-1]
-				if nt != seq[si]:
-					print >> out_eps_open, line,
-				else:
-					# change the nt of seq
-					a[1] = '%.6f' % heights[si]
-					print >> out_eps_open, ' %s' % ' '.join(a)
+            # loop over 4 nt's
+            for i in range(4):
+                line = weblogo_eps_in.readline()
+                a = line.split()
 
-			# move to next nucleotide
-			si += 1
-		else:
-			print >> out_eps_open, line,
+                nt = a[2][1:-1]
+                if nt != seq[si]:
+                    print >> out_eps_open, line,
+                else:
+                    # change the nt of seq
+                    a[1] = '%.6f' % heights[si]
+                    print >> out_eps_open, ' %s' % ' '.join(a)
 
-		# advance to next line
-		line = weblogo_eps_in.readline()
+            # move to next nucleotide
+            si += 1
+        else:
+            print >> out_eps_open, line,
 
-	# clean
-	os.close(fasta_fd)
-	os.remove(fasta_file)
-	os.close(eps_fd)
-	os.remove(eps_file)
+        # advance to next line
+        line = weblogo_eps_in.readline()
+
+    # clean
+    os.close(fasta_fd)
+    os.remove(fasta_file)
+    os.close(eps_fd)
+    os.remove(eps_file)
 
 
 ################################################################################
