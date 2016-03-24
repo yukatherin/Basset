25dbe3803b6e8ea65eb4f13e00a20c0c78776c73
David Kelley <davidkelley44@gmail.com>
2016-03-24 11:56:06 -0400

    more careful typing

--- src/postprocess.lua
+++ src/postprocess.lua
@@ -4,11 +4,11 @@ function troy_norm(p, u)
     local pn = torch.log(torch.cdiv(p,(-p+1)))
     local un
     if type(u) == "number" then
-        un = torch.Tensor((#p)[1],1):fill(torch.log(u/(-u+1)))
+        un = torch.FloatTensor((#p)[1],1):fill(torch.log(u/(-u+1)))
     else
         un = torch.log(torch.cdiv(u,-u+1)):repeatTensor((#p)[1], 1)
     end
-    un = un:float()
+    un = un:float() -- probably unnecessary for all future models
     local zn = pn + xn - un
     return (torch.exp(-zn)+1):pow(-1)
 end
